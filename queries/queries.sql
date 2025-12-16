45650dd6f31eb90315657c7b441b351bae7116b2
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-16 23:04:33 +0100

    refactor: remove USE

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,4 +1,3 @@
-USE universidad;
 -- 1. Retorna un llistat amb el primer cognom, segon cognom i el nom de tots els/les alumnes. El llistat haurà d'estar ordenat alfabèticament de menor a major pel primer cognom, segon cognom i nom.
 SELECT apellido1, apellido2, nombre 
 FROM persona 
