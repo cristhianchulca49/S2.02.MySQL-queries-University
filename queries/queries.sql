e90a4cc98498ffb2f06b8ad615fb2710bd705709
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-16 12:41:51 +0100

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,4 +1,4 @@
-USE universidad;
+
 -- 1. Retorna un llistat amb el primer cognom, segon cognom i el nom de tots els/les alumnes. El llistat haurà d'estar ordenat alfabèticament de menor a major pel primer cognom, segon cognom i nom.
 SELECT apellido1, apellido2, nombre FROM persona WHERE tipo = 'alumno' ORDER BY apellido1, apellido2, nombre;
 
